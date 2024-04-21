956db9b1baca345a055cade77318f2382bb5112b
WesleyKCarlesso <wesleykcarlesso@gmail.com>
2024-04-21 00:11:25 -0300

    Configurando Entity Framework

---
+++ Backend.Infra/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static ModelBuilder SeedData(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User { Id = Guid.NewGuid(), Name = "User Default", Email = "email@backend.com" }
+                );
+
+            return modelBuilder;
+        }
+    }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,13 @@
+using Backend.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BackendContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("BackendDB")).EnableSensitiveDataLogging());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
