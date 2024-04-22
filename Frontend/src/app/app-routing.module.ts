2fbaee316db3a8b109de9503d8dbe19f3126ce1f
theandreluiz <andreluizgaspar1@gmail.com>
2024-04-22 20:00:44 -0300

    add initial calendar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { CommonModule } from '@angular/common';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.compoment';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import { DemoComponent } from './calendar/calendar.component';
 import { Interceptor } from './app.interceptor.module';
 
 @NgModule({
@@ -19,9 +21,14 @@ import { Interceptor } from './app.interceptor.module';
     NavMenuComponent,
     LoginComponent,
     RegisterComponent,
-    CalendarComponent
+    DemoComponent
   ],
   imports: [
+    CommonModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
     BrowserModule,
     HttpClientModule,
     FormsModule,
@@ -30,7 +37,7 @@ import { Interceptor } from './app.interceptor.module';
       { path: 'home', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
-      { path: 'calendar', component: CalendarComponent },
+      { path: 'calendar', component: DemoComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: '**', redirectTo: '/home', pathMatch: 'full' }
     ]),
--- Frontend/src/app/calendar/calendar.component.ts
+++ Frontend/src/app/calendar/calendar.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Subject } from 'rxjs';
+import {
+  CalendarEvent,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  selector: 'mwl-demo-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: 'template.html',
 })
-export class CalendarComponent {
-  isExpanded = false;
+export class DemoComponent {
+  view: CalendarView = CalendarView.Month;
 
-  collapse() {
-    this.isExpanded = false;
-  }
+  viewDate = new Date();
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  events: CalendarEvent[] = [
+    {
+      title: 'Draggable event',
+      color: {primary: '#00ffff', secondary: '#00ffff'},
+      start: new Date(),
+      draggable: true,
+    },
+    {
+      title: 'A non draggable event',
+      color: {primary: '#0000ff', secondary: '#0000ff'},
+      start: new Date(),
+    },
+    {
+      title: 'A non draggable event',
+      color: {primary: '#0000ff', secondary: '#0000ff'},
+      start: new Date(),
+    },
+  ];
+
+  refresh = new Subject<void>();
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd,
+  }: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.refresh.next();
   }
 }
