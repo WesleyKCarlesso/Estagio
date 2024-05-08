b2e9989719d8649841b9baf2e4820f79e23fbec3
TheAndreLuiz <andreluizgaspar1@gmail.com>
2024-05-08 13:52:46 -0300

    testing create

--- Frontend/src/app/components/home/home.compoment.ts
+++ Frontend/src/app/components/home/home.compoment.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { UserDataService } from "../../data-services/user.data-service";
+import { ScheduleDataService } from "../../data-services/schedule.data-service";
+import { MatDatepickerInputEvent } from "@angular/material/datepicker";
 
 @Component({
   selector: "app-home",
@@ -11,21 +12,61 @@ export class HomeComponent implements OnInit {
   isAdmin: boolean = false;
   options = ["1", "2", "3", "4"];
   selectedOption = this.options[0];
+  selectedTime: string = '';
+  date: Date = new Date()
+  userId: string = ''
 
-  constructor(private userDataService: UserDataService) {}
+  constructor(private scheduleDataService: ScheduleDataService) { }
 
   ngOnInit() {
     const userLoggedString = localStorage.getItem("user_logged");
+
     this.isLogged = !!userLoggedString;
 
-    console.log(this.userDataService.getAll());
-
+    console.log('data inicial')
+    console.log(this.date)
     if (!!userLoggedString) {
-      this.isAdmin = JSON.parse(userLoggedString).user.isAdmin;
+      this.userId = JSON.parse(userLoggedString).user.id;
+      console.log('logado:')
+      console.log(this.userId)
     }
+    console.log('get all')
+    this.scheduleDataService.getAll().subscribe((data: any) => {
+      console.log(data)
+    });
   }
 
-  onDropdownChange(option: string) {
+  onDropdownChange(option: Event) {
     console.log(option);
+  }
+
+  onDateChange(option: MatDatepickerInputEvent<any, any>) {
+    console.log(option)
+  }
+
+  onTimeChange(option: Event) {
+    console.log(option)
+  }
+
+  save() {
+    this.date.setMinutes(parseInt(this.selectedTime.split(':')[1]))
+    this.date.setHours(parseInt(this.selectedTime.split(':')[0]))
+    this.scheduleDataService.create({
+      "serviceDate": this.date.toISOString(),
+      "jobId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "userId": this.userId,
+    })
+
+    console.log(this.date.toISOString())
+    console.log('save')
+    console.log('get all')
+    this.scheduleDataService.getAll().subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error('Failed to retrieve data:', error);
+      }
+    )
   }
 }
---
+++ Frontend/src/app/data-services/job.data-service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class UserDataService {
+    module: string = 'https://localhost:7018/api/Job/';
+
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get(this.module + 'GetAll');
+    }
+
+    create(data: any) {
+        const httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+          })
+        };
+        return this.http.post(this.module + 'Create', data, httpOptions);
+    }
+
+    authenticate(data: any) {
+      return this.http.post(this.module + 'Authenticate', data);
+    }
+
+    update(data: any) {
+      return this.http.put(this.module + 'Update', data)
+    }
+}
